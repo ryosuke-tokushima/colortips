2e1977a2dfc802da8f71e2eb120426af78335b24
ryosuke-tokushima <ryosuke.tokushima@gmail.com>
2023-03-26 14:21:03 +0900

    ヘッダーにリンクを実装しました #44

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  get 'logout', to: 'user_sessions#destroy'
 
   resources :users
   resources :posts
