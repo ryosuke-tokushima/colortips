b8fda5ad78e4c2f6e2210d854d31836fd280eb31
ryosuke-tokushima <ryosuke.tokushima@gmail.com>
2023-04-09 23:42:33 +0900

    postテーブルimageカラムのデータ型を変更しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_09_133549) do
+ActiveRecord::Schema.define(version: 2023_04_09_143722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 2023_04_09_133549) do
     t.bigint "color_palette_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.jsonb "images"
+    t.string "images"
     t.index ["color_palette_id"], name: "index_posts_on_color_palette_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
