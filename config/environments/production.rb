7726723a060bc8b7736c06fa76e9a951e2ca3df3
ryosuke-tokushima <ryosuke.tokushima@gmail.com>
2023-04-11 23:52:05 +0900

    プロダクションファイルを修正しました

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # timestamps for the last write to the primary. The resolver uses the context
   # class timestamps to determine how long to wait before reading from the
   # replica.
-  #
+  config.hosts << "colortips.onrender.com"
   # By default Rails will store a last write timestamp in the session. The
   # DatabaseSelector middleware is designed as such you can define your own
   # strategy for connection switching and pass that into the middleware through
