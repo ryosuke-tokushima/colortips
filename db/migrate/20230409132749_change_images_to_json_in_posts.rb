ce73db4888e4e42d3612706500f7c2a5b183c157
ryosuke-tokushima <ryosuke.tokushima@gmail.com>
2023-04-11 13:58:32 +0900

    マイグレーションファイルを修正しました

--- db/migrate/20230409132749_change_images_to_json_in_posts.rb
+++
@@ -1,9 +0,0 @@
-class ChangeImagesToJsonInPosts < ActiveRecord::Migration[6.1]
-  def up
-    change_column :posts, :images, :text
-  end
-
-  def down
-    change_column :posts, :images, :json
-  end
-end
--- db/migrate/20230409150045_change_images_to_jsonb_in_posts_fix.rb
+++
@@ -1,9 +0,0 @@
-class ChangeImagesToJsonbInPostsFix < ActiveRecord::Migration[6.1]
-  def up
-    change_column :posts, :images, 'json USING images::json'
-  end
-
-  def down
-    change_column :posts, :images, :string
-  end
-end
