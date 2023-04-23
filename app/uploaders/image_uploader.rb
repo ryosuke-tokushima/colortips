422a701d8e016f9666fd4f1240291be4b9a32640
ryosuke-tokushima <ryosuke.tokushima@gmail.com>
2023-04-23 11:48:29 +0900

    外部ストレージにS3を使用するための設定を行いました

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,20 +4,16 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  if Rails.env.production?
-    def store_dir
-      "/mnt/colortips-uploads/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
-  else
-    def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
