2c502d269e8ea11c8986b4f506caa536372bc0d2
ryosuke-tokushima <ryosuke.tokushima@gmail.com>
2023-05-11 09:50:33 +0900

    投稿詳細にモーダルウィンドウを実装しました

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,24 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-
-//= require rails-ujs
-//= require activestorage
-//= require common.js
-//= require cable.js
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+import $ from "jquery"
+import Popper from 'popper.js'
+import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all';
 import MagicGrid from 'magic-grid';
-window.MagicGrid = MagicGrid;
 
+window.$ = window.jQuery = $;
+window.MagicGrid = MagicGrid;
 
 Rails.start()
 Turbolinks.start()
@@ -37,5 +31,5 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 $(document).on("click", ".action-buttons .dropdown-menu", function(e){
-	e.stopPropagation();
+    e.stopPropagation();
 });
